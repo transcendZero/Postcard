5ecc67b50f18695ab9daddedf9201d7ad38a69c8
Louis D. Monaghan, Jr <blindaccount1@cox.net>
2015-03-28 21:20:21 -0400

    Name fucntionality added

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -11,8 +11,9 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var messageLabel: UILabel!
-    @IBOutlet weak var enterNameTextField: UITextField!
     @IBOutlet weak var enterMessageTextField: UITextField!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var enterNameTextField: UITextField!
     @IBOutlet weak var mailButton: UIButton!
     
     override func viewDidLoad() {
@@ -31,7 +32,12 @@ class ViewController: UIViewController {
         messageLabel.text = enterMessageTextField.text
         messageLabel.textColor = UIColor.redColor()
         
+        nameLabel.hidden = false
+        nameLabel.text = enterNameTextField.text
+        nameLabel.textColor = UIColor.blueColor()
+        
         enterMessageTextField.text = ""
+        enterNameTextField.text = ""
         enterMessageTextField.resignFirstResponder()
     
         mailButton.setTitle("Mail Sent", forState: UIControlState.Highlighted)
